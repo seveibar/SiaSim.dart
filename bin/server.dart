29389b9ddfa9d16e013f6ca5047995e330a03da7
seveibar <seve700@gmail.com>
2015-04-04 19:14:01 -0400

    cleaned up server

--- bin/server.dart
+++ bin/server.dart
@@ -18,16 +18,19 @@ import "miner.dart";
 import "renter.dart";
 
 void main(List<String> args) {
+  
+  // Parse command line arguments
   var parser = new ArgParser()
-      ..addOption('port', abbr: 'p', defaultsTo: '8080');
+      ..addOption('port', abbr: 'p', defaultsTo: '9980');
 
   var result = parser.parse(args);
-
+  
   var port = int.parse(result['port'], onError: (val) {
     stdout.writeln('Could not parse port value "$val" into a number.');
     exit(1);
   });
-
+  
+  // Initialize the modules for receiving responses
   var daemon = new RegularDaemon();
   var consensus = new RegularConsensus();
   var gateway = new RegularGateway();
@@ -37,37 +40,34 @@ void main(List<String> args) {
   var miner = new RegularMiner();
   var renter = new RegularRenter();
   
+  // Route the URLs to response callbacks
   var route = router()
       ..get("/", (_) => new shelf.Response.ok("SIA SIMULATOR (DART) V0.0.1"))
-      ..get("/daemon/stop", daemon.Stop)
+      ..get("/daemon/stop",         daemon.Stop)
       ..get("/daemon/update/apply", daemon.ApplyUpdate)
       ..get("/daemon/update/check", daemon.CheckForUpdate)
-      ..get("/consensus/status", consensus.Status)
-      ..get("/gateway/status", gateway.Status)
+      ..get("/consensus/status",    consensus.Status)
+      ..get("/gateway/status",      gateway.Status)
       ..get("/gateway/synchronize", gateway.Synchronize)
-      ..get("/gateway/peer/add", gateway.AddPeer)
+      ..get("/gateway/peer/add",    gateway.AddPeer)
       ..get("/gateway/peer/remove", gateway.RemovePeer)
-      ..get("/host/announce", host.Announce)
-      ..get("/host/config", host.Config)
-      ..get("/host/status", host.Status)
-      ..get("/miner/start", miner.Start)
-      ..get("/miner/status", miner.Status)
-      ..get("/miner/stop", miner.Stop)
-      ..get("/renter/download", renter.Download)
-      ..get("/renter/downloadqueue", renter.DownloadQueue)
-      ..get("/renter/files", renter.Files)
-      ..get("/renter/upload", renter.Upload)
+      ..get("/host/announce",       host.Announce)
+      ..get("/host/config",         host.Config)
+      ..get("/host/status",         host.Status)
+      ..get("/miner/start",         miner.Start)
+      ..get("/miner/status",        miner.Status)
+      ..get("/miner/stop",          miner.Stop)
+      ..get("/renter/download",     renter.Download)
+      ..get("/renter/downloadqueue",renter.DownloadQueue)
+      ..get("/renter/files",        renter.Files)
+      ..get("/renter/upload",       renter.Upload)
       ..get("/transactionpool/transactions", transactionPool.Transactions)
-      ..get("/wallet/address", wallet.Address)
-      ..get("/wallet/send", wallet.Send)
-      ..get("/wallet/status", wallet.Status);
+      ..get("/wallet/address",      wallet.Address)
+      ..get("/wallet/send",         wallet.Send)
+      ..get("/wallet/status",       wallet.Status);
   
-  
+  // Actually start the server
   io.serve(route.handler, 'localhost', port).then((server) {
     print('Serving at http://${server.address.host}:${server.port}');
   });
-}
-
-shelf.Response _echoRequest(shelf.Request request) {
-  return new shelf.Response.ok('Request for "${request.url}"');
 }
